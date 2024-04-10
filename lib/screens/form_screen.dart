0d6a7d363268b4586ceba0407eb77bbef2d80f43
TeamCuatro <129485613+TeamCuatro@users.noreply.github.com>
2024-04-10 11:31:48 +0530

    formfield

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vitpair/widgets/text_input_field.dart';
 
 class InterestForm extends StatefulWidget {
   const InterestForm({super.key});
@@ -17,31 +18,36 @@ class _InterestFormState extends State<InterestForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.white12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              controller: _controller,
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  if (_controller.text.length > 0) {
-                    text.add(_controller.text);
-                    _controller.clear();
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("Text is empty"),
-                    ));
-                  }
-                });
-              },
-              child: Text("add"),
-            ),
-            Text(text.isNotEmpty ? text.toString() : ""),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextInputField(
+                icon: Icons.interests_outlined,
+                labelText: "Areas of Interest",
+                controller: _controller,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (_controller.text.length > 0) {
+                      text.add(_controller.text);
+                      _controller.clear();
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text("Text is empty"),
+                      ));
+                    }
+                  });
+                },
+                child: Text("add"),
+              ),
+              Text(text.isNotEmpty ? text.toString() : ""),
+            ],
+          ),
         ),
       ),
     );
